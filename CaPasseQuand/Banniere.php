77bdf93b81683274a8f8d041cc15431840b401b3
ThomasAffre <affre.thomas@gmail.com>
2018-02-15 16:43:49 +0100

    script slider niveau avancée a finir

--- CaPasseQuand/Banniere.php
+++ CaPasseQuand/Banniere.php
@@ -1,20 +1,21 @@
 <div id="banniere_image">
-
-<ul id="listeBanniere">
-	<li>
-		<img src="images/banniere_got.png" alt="Logo" />
-		<div class="fleche_gauche" class="zone_fleche_slider">
+<div id="fleche_gauche" class="zone_fleche_slider" onclick="versgauche()">
 		&#8249
 		</div>
-		<div class="fleche_droite" class="zone_fleche_slider">
+		<div id="fleche_droite" class="zone_fleche_slider">
 		&#8250
-		</div>
-		<div id="banniere_description">
+</div>
+<div id="banniere_description">
 			Game of Thrones 
 			<a href="#" class="bouton_rouge">Voir le calendrier 
 				<img src="images/flecheblanchedroite.png" alt="" />
 			</a>
 		</div>
+<ul id="listeBanniere">
+
+	<li>
+	
+		<img src="images/banniere_got.png" alt="Logo" />
 	<li>
 	<li>
 		<img src="images/B.jpg" alt="Logo" />
--- CaPasseQuand/Js/slider.js
+++ CaPasseQuand/Js/slider.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
-	$(".fleche_gauche").click(function() {
-		var margin = $("listeBanniere").css("margin-left");
-		$("listeBanniere").css("margin-left",margin-950);
+	$("#fleche_gauche").click(function() {
+		var margin = $("#listeBanniere").css("left");
+		var m = margin.substr(0,margin.length-2);
+		margin = parseInt(m,10);
+		$("#listeBanniere").animate({
+			left:margin+950
+		})
+	});
+});
+
+$(document).ready(function(){
+	$("#fleche_droite").click(function() {
+		var margin = $("#listeBanniere").css("left");
+		var m = margin.substr(0,margin.length-2);
+		margin = parseInt(m,10);
+		$("#listeBanniere").animate({
+			left:margin-950
+		})//css("left",margin+950);
+		
 	});
 });
